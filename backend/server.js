c5fc0a52d809f112e14b5df15698d805c08fb23f
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-19 12:58:19 +0200

    mongo logic

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express =require('express')
 const  app=express()
+const Info=require('./Info.model')
 const mongoose=require("mongoose")
+const cors = require('cors');
+app.use(cors())
 app.use(express.json())
 const mongoUrl="mongodb://localhost:27017/login-logic"
-app.post('/login/info',async(req,res)=>{
-    const info =await
+app.post('/login/info', async (req, res) => {
+    try {
+        const info = await Info.create(req.body);
+        res.status(200).json(info); 
+        console.log("Saved to DB:", info);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 })
 
 mongoose.connect(mongoUrl)
