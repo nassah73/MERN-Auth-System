593fc7cf0aa402946fc42c19c3b0571c616dae59
HASSAN ES-SEBAIY <hassansobai6@gmail.com>
2026-04-19 12:59:47 +0200

    remove log

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.post('/login/info', async (req, res) => {
     try {
         const info = await Info.create(req.body);
         res.status(200).json(info); 
-        console.log("Saved to DB:", info);
+        
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
